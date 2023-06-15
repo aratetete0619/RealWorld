75f7a1a0c09ae06c65b41f85b8549a6453b27ab1
aratetete0619 <ara.ct0619.ct@gmail.com>
2023-06-16 02:44:17 +0900

    Fix rubocop.yml and authentication_controller.rb

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,7 +1,7 @@
 class AuthenticationController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       render json: { token: user.generate_jwt }, status: :ok
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
